f76530166b4db65a02c4dc784f381a2fe416abbf
fadilzhar <fadilathazharif@gmail.com>
2024-10-22 08:36:32 +0700

    First

---
+++ lib/model/splash_data.dart
@@ -0,0 +1,15 @@
+List<Map<String, String>> splashData = [
+  {
+    "text": "Hello There! Welcome to Easy Mart! \n Let's shop!",
+    "image": "assets/images/splash_1.png"
+  },
+  {
+    "text":
+        "We help people connect with store \naround United State of America",
+    "image": "assets/images/splash_2.png"
+  },
+  {
+    "text": "We show the easy way to shop. \nJust stay at home with us",
+    "image": "assets/images/splash_3.png"
+  },
+];
